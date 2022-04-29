24af97afc48c810de113885ef7f827cc009072a6
zoya_admin <zoyabogomolova@gmail.com>
2022-04-29 10:19:11 +0200

    added palimdrome checker page

--- is_palindrome.js
+++ is_palindrome.js
@@ -1,20 +1,24 @@
 
-function isPalindrom( word ) {
+function checkPalindrom() {
 
+    let word = document.getElementById("my-text").value;
+    
+    document.getElementById("response").innerHTML = '<h3 id="greeting">' + word +' '+ isPalindrom( word.toLowerCase() ) + '!</h3>';
+}
+
+function isPalindrom( word ) {
 	let i = 0
 	let j = word.length - 1
-
 	while ( i < j ) {
 		if ( word.charAt(i) != word.charAt(j) ) {
-			return false
+			return "isn't palindrome"
 		}
 		i++
 		j--
 	}
-
-	return true
+	return "is palindrome"
 }
 
-console.log( isPalindrom( "madam" ))
-console.log( isPalindrom( "boob" ))
-console.log( isPalindrom( "Zoia" ))
+//console.log( isPalindrom( "madam" ))
+//console.log( isPalindrom( "boob" ))
+//console.log( isPalindrom( "Zoia" ))
